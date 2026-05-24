d94cce40312017a1871c446a0dd2172a063b4518
sahal <rashidsahal222@gmail.com>
2026-05-24 10:24:54 +0530

    connected live backend api

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import Contact from "./Pages/Contact";
 // Components
 import ScrollToTop from "./Components/ScrollToTop";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://fundbridge-backend-rashid.onrender.com";
 
 function App() {
   const [campaigns, setCampaigns] = useState([]);
